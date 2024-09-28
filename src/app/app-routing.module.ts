59a43e29bb702bcf094c4b756d1f71d1149f253a
everpe <ever.penab@autonoma.edu.co>
2024-09-27 21:17:18 -0500

    add auth guard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -15,11 +16,13 @@ const routes: Routes = [
       },
       {
         path: 'dashboard',
+        canActivate: [authGuard],
         loadChildren: () =>
           import('./pages/pages.module').then((m) => m.PagesModule),
       },
       {
         path: 'ui-components',
+        canActivate: [authGuard],
         loadChildren: () =>
           import('./pages/ui-components/ui-components.module').then(
             (m) => m.UicomponentsModule
@@ -27,6 +30,7 @@ const routes: Routes = [
       },
       {
         path: 'extra',
+        canActivate: [authGuard],
         loadChildren: () =>
           import('./pages/extra/extra.module').then((m) => m.ExtraModule),
       },
