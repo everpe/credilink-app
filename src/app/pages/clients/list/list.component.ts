f29caddf6dadfb19cce5bb8f75275b9675dc40ef
everpe <ever.penab@autonoma.edu.co>
2024-09-29 11:37:19 -0500

    ajustes en la paginacion y problemas con deteccion cambios paginador entre pages

--- src/app/pages/clients/list/list.component.ts
+++ src/app/pages/clients/list/list.component.ts
@@ -1,11 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { MatMenuItem, MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ClientService } from 'src/app/services/clients/client.service';
 
@@ -28,16 +27,13 @@ import { ClientService } from 'src/app/services/clients/client.service';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  clientsDatasource = new MatTableDataSource<any>();
-  offset = 1;
+export class ListComponent implements AfterViewInit {
+  offset = 0;
   limit = 10;
   sede = 1;
   search = 'otro';
   totalClients = 0;
-
+  
   // Columnas a mostrar en la tabla
   displayedColumns: string[] = [
     'first_name',
@@ -45,47 +41,51 @@ export class ListComponent {
     'type_document',
     'document_number',
     'city',
-    'mobile',
+    // 'mobile',
     'status',
     'actions' 
   ];
-  constructor(private clientService: ClientService) {}
+  
+  private paginator!: MatPaginator;
+  clientsDatasource = new MatTableDataSource<any>();
 
-  ngOnInit(): void {
+  constructor(private clientService: ClientService) {
+    }
+
+  ngAfterViewInit(): void {
+    this.clientsDatasource.paginator = this.paginator;
     this.getClients();
   }
 
   // Método para obtener los clientes con paginación
   getClients(): void {
     this.clientService.getClients(this.offset, this.limit, this.sede, this.search).subscribe(
-      response => {
-        this.clientsDatasource.data = response.results;  // Ajusta según el formato de la respuesta
-        this.clientsDatasource.paginator = this.paginator;  // Vincula el paginador a los datos
-        this.totalClients = response.count;
-        this.clientsDatasource.sort = this.sort;  // Vincula el ordenamiento
-        console.log(response.count);
+      (response) => {
+        this.clientsDatasource.data = response.results;  // Asigna los datos obtenidos de la API
+        this.totalClients = response.count;  // Actualiza el número total de clientes
       },
-      error => {
+      (error) => {
         console.error('Error al obtener los clientes:', error);
       }
     );
   }
+  
 
-  // Método para manejar cambios en la paginación
   onPageChange(event: PageEvent): void {
-    this.offset = event.pageIndex * event.pageSize;
+    this.offset = ((event.pageIndex + 1) - 1) * event.pageSize;// event.pageIndex * event.pageSize;
+    // console.log('pageIndex;' + event.pageIndex)
+    // console.log('pageSize;' + event.pageSize)
+    // console.log('offset;' + this.offset)
     this.limit = event.pageSize;
     this.getClients();  // Vuelve a llamar al servicio para obtener nuevos datos
   }
 
   editClient(client: any): void {
     console.log('Editar cliente:', client);
-    // Aquí va la lógica para editar el cliente
   }
 
   // Método para eliminar cliente
   deleteClient(client: any): void {
     console.log('Eliminar cliente:', client);
-    // Aquí va la lógica para eliminar el cliente
   }
 }
